e0b063d1050bb9a205d9484a46255b9a0f1ab13a
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2018-03-16 08:51:27 +0100

    Build scripts

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+echo Executing after success scripts on branch $TRAVIS_BRANCH
+echo Triggering MyGet package build
+
+cd src/DShop.Services.Storage.Models
+dotnet pack /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER --no-restore -o .
+
+echo Uploading DShop.Services.Storage.Models package to MyGet using branch $TRAVIS_BRANCH
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/dnc-dshop/api/v2/package
+    ;;
+  "develop")
+    dotnet nuget push *.nupkg -k $MYGET_DEV_API_KEY -s https://www.myget.org/F/dnc-dshop-dev/api/v2/package
+    ;;    
+esac
+
+cd ../../
 dotnet publish --no-restore ./src/DShop.Services.Storage -c Release -o ./bin/Docker
 
 DOCKER_ENV=''
