36fe2eada7f8c7d424ef89b855dd5d6664bbd83b
GooRiOn <foreverframenet@gmail.com>
2018-03-18 17:31:21 +0100

    Fixed bug with browse products

--- src/DShop.Services.Storage.Models/Queries/BrowseProducts.cs
+++ src/DShop.Services.Storage.Models/Queries/BrowseProducts.cs
@@ -7,5 +7,11 @@ namespace DShop.Services.Storage.Models.Queries
         public string Vendor { get; set; }
         public decimal PriceFrom { get; set; }
         public decimal PriceTo { get; set; }
+
+        public BrowseProducts()
+        {
+            Vendor = string.Empty;
+            PriceTo = decimal.MaxValue;
+        }
     }
 }
