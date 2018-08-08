acc6a8fefbc074cc60ded02cad2bfb06a0e29a0a
Piotr Gankiewicz <spetz90@gmail.com>
2018-08-08 12:22:11 +0200

    Fixed description name

--- src/DShop.Services.Storage.Models/Products/Product.cs
+++ src/DShop.Services.Storage.Models/Products/Product.cs
@@ -7,7 +7,7 @@ namespace DShop.Services.Storage.Models.Products
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Descirption { get; set; }
+        public string Description { get; set; }
         public string Vendor { get; set; }
         public decimal Price { get; set; }        
     }
